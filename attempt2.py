cab2b8ae3ee4a5030ab1badf649d9f722a91f489
Fumino <53288288+blazecritter45@users.noreply.github.com>
2021-02-21 14:23:15 +0530

    Update attempt2.py

--- attempt2.py
+++ attempt2.py
@@ -23,3 +23,16 @@ def displayBoard(hang, missedLetters, correctLetters, secretWord):
     for letter in blanks:  # show the secret word with spaces in between each letter
         print(letter, end=' ')
     print("\n")
+    
+    def getGuess(alreadyGuessed):
+    while True:
+        guess = input('Guess a letter: ')
+        guess = guess.lower()
+        if len(guess) != 1:
+            print('Please enter a single letter.')
+        elif guess in alreadyGuessed:
+            print('You have already guessed that letter. Choose again.')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Please enter a LETTER.')
+        else:
+            return guess
