478045def6891958db070d7347ff97adaf3a7d0c
Fumino <53288288+blazecritter45@users.noreply.github.com>
2021-02-21 14:21:54 +0530

    Update attempt2.py

--- attempt2.py
+++ attempt2.py
@@ -5,3 +5,13 @@ def getRandomWord():
 
     word = random.choice(words)
     return word
+def displayBoard(hang, missedLetters, correctLetters, secretWord):
+    print(hang[len(missedLetters)])
+    print()
+
+    print('Missed Letters:', end=' ')
+    for letter in missedLetters:
+        print(letter, end=' ')
+    print("\n")
+
+    blanks = '_' * len(secretWord)
